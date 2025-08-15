7772b5176a26845fad3b4e8fba144f62c5610e2a
Rebeca Colares <rebecacb.dev@gmail.com>
2025-08-15 18:32:49 -0300

    update
    Alterações feitas de forma a completar o desafio proposto pela ONE, de incrementar funções no arquivo javascript para fazer o front-end funcionar, adicionar lista e sortear amigos.

--- app.js
+++ app.js
@@ -1,1 +1,55 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let listaNomes = [];
+
+function textoVazio() {
+    alert('Atenção! O texto não pode estar vazio. Por favor, insira um nome válido.');
+}
+
+
+function adicionarAmigo() {
+    const input = document.getElementById('amigo');
+    const nome = input.value.trim(); 
+
+    if (!nome) {
+        textoVazio();
+        return;
+    } else {
+        listaNomes.push(nome);
+        console.log(listaNomes);
+        atualizarLista();
+        limparCampo();
+    }
+}
+
+function atualizarLista() {
+    const ul = document.getElementById('listaAmigos');
+    ul.innerHTML = '';
+    for (let i = 0; i < listaNomes.length; i++) {
+        const li = document.createElement('li');
+        li.textContent = listaNomes[i];
+        ul.appendChild(li);
+    }
+    
+}
+
+function limparCampo() {
+    const input = document.getElementById('amigo');
+    input.value = '';
+}
+
+function sortearAmigo() {
+    if (listaNomes.length < 1) {
+        alert('Você não inseriu nomes o suficiente!');
+        return;
+    } else {
+
+        const ulLista = document.getElementById('listaAmigos');
+        ulLista.innerHTML = ''; 
+
+        const indiceSorteado = Math.floor(Math.random() * listaNomes.length);
+        const nomeSorteado = listaNomes[indiceSorteado];
+
+        const li = document.createElement('li');
+        li.textContent = nomeSorteado;
+        ulLista.appendChild(li);
+    }
+}
